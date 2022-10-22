bbd16989f2eda97f5b8c7a0583847aaa51dae1fd
Alejandro Acero <alejandroacero2012@gmail.com>
2022-10-22 01:09:06 -0500

    Finished for now

---
+++ script.js
@@ -0,0 +1,21 @@
+// let elems = document.getElementsByClassName("img2")
+
+// for (var i = 0; i < elems.length; i++){
+//     elems[i].style.height = document.getElementById("js").clientHeight+"px"
+// }
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
